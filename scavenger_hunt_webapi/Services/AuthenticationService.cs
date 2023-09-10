fb6f205e8afc57ee51caa4925c0e15bc66e0a9b0
Harry Ross <hazross@hotmail.com>
2023-09-10 15:20:08 +1000

    Moved entities to new folder, added JWT generation

--- scavenger_hunt_webapi/Services/AuthenticationService.cs
+++ scavenger_hunt_webapi/Services/AuthenticationService.cs
@@ -1,11 +1,13 @@
-﻿using Microsoft.IdentityModel.Tokens;
+﻿using Microsoft.AspNetCore.Components.Forms;
+using Microsoft.IdentityModel.Tokens;
 using scavenger_hunt_webapi.Models;
 using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace scavenger_hunt_webapi.Services;
 public class AuthenticationService
 { 
-    private static readonly TimeSpan TokenTime = TimeSpan.FromHours(4);
 
     private readonly IConfiguration _configuration;
     public AuthenticationService(IConfiguration configuration)
@@ -14,15 +16,30 @@ public class AuthenticationService
     }
     private string GenerateJWT(User user)
     {
-        var secret = _configuration["JWT:Secret"];
+        string secret = _configuration["JWT:Secret"]!;
 
         var issuer = _configuration["JWT:Issuer"];
         var audience = _configuration["JWT:Audience"];
+
+        var expirationTime = DateTime.UtcNow.AddHours(4);
+
+        var tokenDescriptor = new SecurityTokenDescriptor
+        {
+            Subject = new ClaimsIdentity(new[]
+            {
+                new Claim(ClaimTypes.Name, user.Id)
+            }),
+            Expires = expirationTime,
+            SigningCredentials = new SigningCredentials(
+                new SymmetricSecurityKey(Encoding.UTF8.GetBytes(secret)),
+                SecurityAlgorithms.HmacSha256Signature
+            )
+        };
 
         var token = new JwtSecurityToken(issuer, audience);
 
         var jwt = new JwtSecurityTokenHandler().WriteToken(token);
 
-        return "";
+        return jwt;
     }
 }
